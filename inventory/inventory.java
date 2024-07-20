02f5bb7e564d40df7939c2f56ba3a557e9a54e6e
foreverknight1455 <sreyasinemily@gmail.com>
2024-07-20 12:41:52 +0530

    added changes in inventory.java

---
+++ inventory/inventory.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+class item{
+    String name;
+    float price;
+}
+
+class ui{
+    
+}
+
+class inventory{
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+}
